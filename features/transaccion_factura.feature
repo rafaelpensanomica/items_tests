8ef6213539804f64b9ee8d2a28164f1cc786ccfb
Rafael Williams <rwilliams@pensanomica.com>
2018-06-11 08:47:15 -0500

    mejoras

---
+++ features/transaccion_factura.feature
@@ -0,0 +1,9 @@
+Feature: Transaccion de Factura de compras
+ cuando cambio el estado de la factura a por pagar
+ I quiero poder hacer la Transaccion
+ Scenario: Factura estado por pagar
+  Given precondition
+   When action
+   Then testable outcome
+  
+ 
--- src/RedondeoV1.php
+++ src/RedondeoV1.php
@@ -13,14 +13,14 @@ class RedondeoV1 extends Filas{
         $resultados = $this->filas->map(function($item) use($decimales){
 
             $precio = $item['cantidad'] * $item['precio'] ;
-            $item['subTotal'] = $precio;
+            $item['subTotal'] = round($precio, 2,PHP_ROUND_HALF_UP);
             $descuento = $precio * $item['descuento'];
-            $tem_desc = round($descuento * $decimales);
+            $tem_desc = round($descuento * $decimales, 2, PHP_ROUND_HALF_UP);
             $item['descuento_total'] = round(($tem_desc/$decimales),4,PHP_ROUND_HALF_UP);
-            $impuesto = ($precio -  $item['descuento_total']) * $item['impuesto'];
-            $temp_imp =  round($impuesto * $decimales);
-            $item['impuesto_total'] = round(($temp_imp / $decimales),2,PHP_ROUND_HALF_UP);
-            $item['retenido'] = round(($item['impuesto_total'] / 2), 2, PHP_ROUND_HALF_UP);
+            $impuesto = ($item['subTotal'] -  $item['descuento_total']) * $item['impuesto'];
+            $temp_imp =  round(($impuesto * $decimales), 4, PHP_ROUND_HALF_UP);
+            $item['impuesto_total'] = round(($temp_imp / $decimales),4,PHP_ROUND_HALF_UP);
+            $item['retenido'] = round(($item['impuesto_total'] * 0.50), 4, PHP_ROUND_HALF_UP);
            
             return $item;
         });
